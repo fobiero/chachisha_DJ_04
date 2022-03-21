a211a8844ee240c8288f6e855c7408595f4ce4cf
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 00:08:30 +0300

    query db

--- chachisha/views.py
+++ chachisha/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 posts = [
     {
         'title' : 'Post One', 
         'author' : 'post_author',
+        'image_upload': 'Uploaded Images goes here!',
         'content' : 'this content is for post one',
         'date_posted' : '21st Jan 2018',
     },
     {
         'title' : 'Post Two', 
         'author' : 'post_author - 2',
+        'image_upload': 'Uploaded Images goes here!',
         'content' : 'this content is for post two only',
         'date_posted' : '30th Jan 2018',
     }
@@ -21,7 +24,7 @@ def index(req) :
 
 def home(req):
     data = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
 
     return render(req,'home.html',data)
