ec8accb602ca930dea575aaaa127678535a107b0
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 00:18:13 +0300

    change date format using pipes

--- chachisha/views.py
+++ chachisha/views.py
@@ -2,26 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts = [
-    {
-        'title' : 'Post One', 
-        'author' : 'post_author',
-        'image_upload': 'Uploaded Images goes here!',
-        'content' : 'this content is for post one',
-        'date_posted' : '21st Jan 2018',
-    },
-    {
-        'title' : 'Post Two', 
-        'author' : 'post_author - 2',
-        'image_upload': 'Uploaded Images goes here!',
-        'content' : 'this content is for post two only',
-        'date_posted' : '30th Jan 2018',
-    }
-]
+# posts = [
+#     {
+#         'title' : 'Post One', 
+#         'author' : 'post_author',
+#         'image_upload': 'Uploaded Images goes here!',
+#         'content' : 'this content is for post one',
+#         'date_posted' : '21st Jan 2018',
+#     },
+#     {
+#         'title' : 'Post Two', 
+#         'author' : 'post_author - 2',
+#         'image_upload': 'Uploaded Images goes here!',
+#         'content' : 'this content is for post two only',
+#         'date_posted' : '30th Jan 2018',
+#     }
+# ]
 
 def index(req) :
     return render (req, 'index.html')
 
+# @TODO:get all posts 
 def home(req):
     data = {
         'posts' : Post.objects.all()
@@ -29,6 +30,7 @@ def home(req):
 
     return render(req,'home.html',data)
 
+# @TODO:get user credentials 
 def profile(req):
 
     return render(req,'profile.html')
