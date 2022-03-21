63710f9de5a5eff5fab36f76a977ee65018a3d7e
feLycks <felix.obiero@student.moringaschool.com>
2022-03-21 19:32:35 +0300

    dummy sample data

--- chachisha/views.py
+++ chachisha/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'title' : 'Post One', 
+        'author' : 'post_author',
+        'content' : 'this content is for post one',
+        'date_posted' : '21st Jan 2018',
+    },
+    {
+        'title' : 'Post Two', 
+        'author' : 'post_author - 2',
+        'content' : 'this content is for post two only',
+        'date_posted' : '30th Jan 2018',
+    }
+]
+
+
 def home(req):
-    return render(req,'index.html')
+    data = {
+        'posts' : posts
+    }
+
+    return render(req,'index.html',data)
 
 def profile(req):
-    return render(req,'profile.html')
+    title = 'Profile'
+
+    return render(req,'profile.html', title=title)
