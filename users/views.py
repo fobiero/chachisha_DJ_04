67ef020c4edce7d601b8fd58036c166b7fe7049c
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 01:44:46 +0300

    debug register page & logic

--- users/views.py
+++ users/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Hi {username}, Your Account has been Created!')
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegForm()
 
     return render (request, 'register.html', {'form':form})
 
