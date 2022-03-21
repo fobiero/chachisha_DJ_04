3de20adca1c124e936b0606f8551598f9a9f1a3b
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 00:27:47 +0300

    first Post attemp

--- chachisha/views.py
+++ chachisha/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-# posts = [
-#     {
-#         'title' : 'Post One', 
-#         'author' : 'post_author',
-#         'image_upload': 'Uploaded Images goes here!',
-#         'content' : 'this content is for post one',
-#         'date_posted' : '21st Jan 2018',
-#     },
-#     {
-#         'title' : 'Post Two', 
-#         'author' : 'post_author - 2',
-#         'image_upload': 'Uploaded Images goes here!',
-#         'content' : 'this content is for post two only',
-#         'date_posted' : '30th Jan 2018',
-#     }
-# ]
-
 def index(req) :
     return render (req, 'index.html')
 
