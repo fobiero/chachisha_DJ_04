0ed55e4453629253aeda1b08181b7a5e504bb901
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 01:24:34 +0300

    auth/logic build

--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-def register(req):
-    form = UserCreationForm()
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, Your Account has been Created!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
 
-    return render (req, 'register.html', {'form':form})
+    return render (request, 'register.html', {'form':form})
 
-def login(req):
-    return render (req, 'login.html')
