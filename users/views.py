4ee858a6643d07c7017dffbc7fbb69aeac54b008
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 07:51:34 +0300

    updated user Profile

--- users/views.py
+++ users/views.py
@@ -21,16 +21,17 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        u_prof = UpdateProfile(request.POST, instance=request.user)
-        u_img = UpdateImage(request.POST,request.FILES, instance=request.user.profile)
-        if u_prof.is_valid() and u_img.is_valid():
-            u_prof.save()
+        u_img = UpdateImage(request.POST,instance=request.user)
+        u_prof = UpdateProfile(request.POST, request.FILES, instance=request.user.profile)
+        if u_img.is_valid() and u_prof.is_valid():
             u_img.save()
-            messages.success(request, f'Account Updated Successfully!')
+            u_prof.save()
+            messages.success(request, f'Account Updated!')
             return redirect('profile')
     else:
-        u_prof = UpdateProfile(instance=request.user)
-        u_img = UpdateImage(request.user.profile)
+        u_img = UpdateImage(instance=request.user)
+        u_prof = UpdateProfile(instance=request.user.profile)
+
     context = {
         'u_prof': u_prof,
         'u_img' : u_img
