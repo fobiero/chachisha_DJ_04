7f092d063045aed455ac4a3593dfbabc0ad9204f
feLycks <felix.obiero@student.moringaschool.com>
2022-03-22 07:08:39 +0300

    prupdate profile Logic build

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegForm
+from .forms import UserRegForm, UpdateImage, UpdateProfile
 
 # @TODO:registrer page 
 def register(request):
@@ -20,7 +20,22 @@ def register(request):
 # @TODO:get user credentials 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        u_prof = UpdateProfile(request.POST, instance=request.user)
+        u_img = UpdateImage(request.POST,request.FILES, instance=request.user.profile)
+        if u_prof.is_valid() and u_img.is_valid():
+            u_prof.save()
+            u_img.save()
+            messages.success(request, f'Account Updated Successfully!')
+            return redirect('profile')
+    else:
+        u_prof = UpdateProfile(instance=request.user)
+        u_img = UpdateImage(request.user.profile)
+    context = {
+        'u_prof': u_prof,
+        'u_img' : u_img
+    }
 
-    return render(request,'profile.html')
+    return render(request,'profile.html', context)
 
 
