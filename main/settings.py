d40017fb07cd65db0475c633459f5b04ea482553
feLycks <felix.obiero@student.moringaschool.com>
2022-03-23 17:46:15 +0300

    debug db

--- main/settings.py
+++ main/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', False)
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'ENGINE': 'django.db.backends.postgresql',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
